973b3055dc0b4a8dbe80fe03595465f17e9a6997
TheAgent-1 <108107690+TheAgent-1@users.noreply.github.com>
2024-08-26 10:24:28 +1200

    added lcd starter

---
+++ MiniGPSRadio/Arduino/260824/260824.ino
@@ -0,0 +1,101 @@
+// Board: XIAO_ESP32C3
+
+
+//Libraries============================
+#include <SoftwareSerial.h>       // Prerequisite 1
+#include <SPI.h>                  // Prerequisite 2
+
+#include <TinyGPS++.h>            // GPS
+#include <LiquidCrystal_I2C.h>    // LCD I2C
+#include <nRF24L01.h>             // NRF24L01 - 1
+#include <RF24.h>                 // NRF24L01 - 2
+//Libraries=END========================
+
+//Variables============================
+// Main
+const bool Receiver = false;
+
+// GPS
+const int GPS_TX = 0;
+const int GPS_RX = 0;
+const int GPSBaud = 9600;
+
+// LCD I2C
+const int Cols = 16;
+const int Rows = 2;
+
+// NRF24L01
+const int NRF_CE = 0;
+const int NRF_CSN = 0;
+const byte NRF_address[6] = "5EGU1";
+char data[32] = "placeholder";
+char buffer[32] = {0};
+
+//Variables=END========================
+
+//Objects==============================
+// GPS
+SoftwareSerial gpsSerial(GPS_RX, GPS_TX);
+TinyGPSPlus gps;
+
+// LCD I2C
+LiquidCrystal_I2C lcd(0x27, Cols, Rows);
+
+// NRF24L01
+RF24 radio(NRF_CE, NRF_CSN);
+//Objects=END==========================
+
+//Main=Code============================
+void setup() {
+  Serial.begin(115200);
+  GPS_Setup();
+  NRF_Setup();
+}
+
+void loop() {
+
+}
+//Main=Code=END========================
+
+//Secondary=Code=======================
+void GPS_Setup() {
+  gpsSerial.begin(GPSBaud);
+  Serial.println("Started: GPS Communication");
+}
+
+void GPS_Loop() {
+  while (gpsSerial.available() > 0) {
+    if (gps.encode(gpsSerial.read())) {
+      
+    }
+  }
+
+  if (millis() > 5000 && gps.charsProcessed() < 10) {
+    Serial.println("No GPS");
+    while (true)
+      ;
+  }
+}
+
+
+void NRF_Setup() {
+  radio.begin();
+
+  if (!Receiver) {
+    radio.openWritingPipe(NRF_address);
+    radio.stopListening();
+    Serial.println("Started: NRF Radio");
+    Serial.println("NRF mode: Transmitter");
+  } else {
+    radio.openReadingPipe(0, NRF_address);
+    radio.startListening();
+    Serial.println("Started: NRF Radio");
+    Serial.println("NRF mode: Receiver");
+  }
+}
+
+void NRF_Loop() {
+  //placeholder
+}
+
+//Secondary=Code=END===================
---
+++ MiniGPSRadio/Arduino/260824/Extra.h
@@ -0,0 +1,7 @@
+#include <Arduino.h>
+#include <WString.h>
+
+class Extra {
+  public:
+  
+}
